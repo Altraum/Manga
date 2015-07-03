793aae5212fb15046ff1e920a39eb150ef09b8fe
Altraum <great.ore@gmail.com>
2015-07-03 17:07:26 -0400

    Panels added to left and right of image that resize to maintain image aspect ratio.

--- src/manga/Manga.java
+++ src/manga/Manga.java
@@ -19,6 +19,7 @@ import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.net.URL;
+import java.math.*;
 
 public class Manga extends JFrame implements ActionListener {
 
@@ -28,6 +29,8 @@ public class Manga extends JFrame implements ActionListener {
     
     public static Document doc = null;
     public static JPanel pane = new JPanel();
+    public static JPanel padLeft = new JPanel();
+    public static JPanel padRight = new JPanel();
     public Manga(){
         this.setLayout(new BorderLayout());
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -53,6 +56,9 @@ public class Manga extends JFrame implements ActionListener {
                         System.out.println(attr);
                         URL url = new URL(attr);
                         BufferedImage image = ImageIO.read(url);
+                        int h = image.getHeight();
+                        int w = image.getWidth();
+                        int r = ratioWidth(w, h);
                         Manga.this.remove(pane);
                         pane.removeAll();
                         pane = new JPanel() {
@@ -62,6 +68,9 @@ public class Manga extends JFrame implements ActionListener {
                                 g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
                             }
                         };
+                        Dimension d = new Dimension(((1920 - r)/2),h);
+                        padLeft.setPreferredSize(d);
+                        padRight.setPreferredSize(d);
                         Manga.this.add(pane);
                         Manga.this.revalidate();
                     }
@@ -85,6 +94,9 @@ public class Manga extends JFrame implements ActionListener {
                         System.out.println(attr);
                         URL url = new URL(attr);
                         BufferedImage image = ImageIO.read(url);
+                        int h = image.getHeight();
+                        int w = image.getWidth();
+                        int r = ratioWidth(w, h);
                         pane.removeAll();
                         Manga.this.remove(pane);
                         pane = new JPanel() {
@@ -94,6 +106,10 @@ public class Manga extends JFrame implements ActionListener {
                                 g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
                             }
                         };
+                        System.out.println(r);
+                        Dimension d = new Dimension(((1920 - r)/2),h);
+                        padLeft.setPreferredSize(d);
+                        padRight.setPreferredSize(d);
                         Manga.this.add(pane);
                         Manga.this.revalidate();
                     }
@@ -103,6 +119,26 @@ public class Manga extends JFrame implements ActionListener {
                 }
             }
         );
+    }
+    
+    public int ratioWidth(int w, int h){
+        int rWidth;
+        float r;
+        if ( h > 968){
+            r = (float)968/h;
+        }
+        else if ( h < 968){
+            r = h/(float)968;
+        }
+        else{
+            r = 1;
+        }
+        rWidth = Math.round(w * r);
+        System.out.println("Height: " + h);
+        System.out.println("Computer's ratio: " + r);
+        System.out.println("Width: " + w);
+        System.out.println("New Width: " + rWidth);
+        return rWidth;
     }
     
     public void actionPerformed(ActionEvent e) {
@@ -139,36 +175,14 @@ public class Manga extends JFrame implements ActionListener {
                 g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
             }
         };
+        Dimension d = new Dimension((1920 - w)/2,h);
+        padLeft.setBackground(Color.black);
+        padRight.setBackground(Color.black);
+        padLeft.setPreferredSize(d);
+        padRight.setPreferredSize(d);
+        window.add(padLeft, BorderLayout.WEST);
+        window.add(padRight, BorderLayout.EAST);
         window.add(pane, BorderLayout.CENTER);
         window.setVisible(true);
-        //String img = (attr.substring( attr.indexOf("http://"), attr.indexOf(")") ) );
-        /*Element column = row.select("td>a").first();
-        String chapters = column.attr("href");
-        System.out.println(chapters);
-        /*Document doc = Jsoup.connect("http://bato.to/read/_/11478/kingdom_v1_ch1_by_exertus-scans/3").get();  
-        String title = doc.title();  
-        System.out.println("Title is: " + title);
-        Element e = doc.select("img#comic_page").first();
-        System.out.println(e.attr("src"));
-        String attr = e.attr("src");
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        
-        
-        URL url = new URL(attr);
-        BufferedImage image = ImageIO.read(url);
-        int h = image.getHeight();
-        int w = image.getWidth();
-        frame.setSize(w, h);
-        JPanel pane = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(image, 0, 0, null);
-            }
-        };
-        frame.add(pane);
-        frame.setVisible(true);*/
     }
-    
 }
